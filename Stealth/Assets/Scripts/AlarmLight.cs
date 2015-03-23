28fef78f8e445a4e691b8f726b845d36e709cbe2
Peter Corazao <pcorazao@nc2media.com>
2015-03-23 12:17:20 -0500

    added light script.

---
+++ Stealth/Assets/Scripts/AlarmLight.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlarmLight : MonoBehaviour {
+
+	public float fadeSpeed = 2f;
+	public float highIntensity = 2f;
+	public float lowIntenssity = 0.5f;
+	public float changeMargin = 0.2f;
+	public bool alarmOn;
+
+	private Light light;
+	private float targetIntensity;
+
+	void Awake()
+	{
+		light = this.gameObject;
+		light.intensity = 0f;
+		targetIntensity = highIntensity;
+	}
+
+	void Update()
+	{
+		if (alarmOn) {
+			light.intensity = Mathf.Lerp (light.intensity, targetIntensity, fadeSpeed * Time.deltaTime);
+		} else {
+			light.intensity = Mathf.Lerp (light.intensity, 0f, fadeSpeed * Time.deltaTime);
+		}
+	}
+
+	void CheckTargetIntensity()
+	{
+		if (Mathf.Abs (targetIntensity - light.intensity) < changeMargin) {
+			if(targetIntensity == highIntensity)
+			{
+				targetIntensity = lowIntenssity;
+			}else{
+				targetIntensity = highIntensity;
+			}
+		}
+	}
+}
