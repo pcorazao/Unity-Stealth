723dd28d012c7f2f7a0058814e8253be01f16e73
Peter Corazao <pcorazao@nc2media.com>
2015-03-23 14:53:24 -0500

    added camera animation.

---
+++ Stealth/Assets/Scripts/CCTVPlayerDetection.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CCTVPlayerDetection : MonoBehaviour {
+
+	private GameObject player;
+	private LastPlayerSighting lastPlayerSighting;
+
+	void Awake()
+	{
+		player = GameObject.FindGameObjectWithTag (Tags.player);
+		lastPlayerSighting = GameObject.FindGameObjectWithTag (Tags.gameController).GetComponent<LastPlayerSighting> ();
+	}
+
+	void OnTriggerStay(Collider other)
+	{
+		if (other.gameObject == player) {
+			Vector3 relPlayerPos = player.transform.position - transform.position;
+			RaycastHit hit;
+
+			if(Physics.Raycast(transform.position, relPlayerPos,out hit))
+			{
+				if(hit.collider.gameObject == player)
+				{
+					lastPlayerSighting.position = player.transform.position;
+				}
+			}
+		}
+	}
+}
