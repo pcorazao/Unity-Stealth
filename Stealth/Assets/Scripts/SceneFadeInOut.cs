d4e331a17079eaa91c481e3b4586b77b3b5353b6
Peter Corazao <pcorazao@nc2media.com>
2015-03-23 12:58:24 -0500

    added screen fader

---
+++ Stealth/Assets/Scripts/SceneFadeInOut.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneFadeInOut : MonoBehaviour {
+
+	public float fadeSpeed = 1.5f;
+
+	private bool sceneStarting = true;
+	private GUITexture guiTexture;
+
+	void Awake(){
+		var guiTextures = this.gameObject.GetComponents<GUITexture> ();
+		var guiTexture = guiTextures [0];
+
+		guiTexture.pixelInset = new Rect (0f, 0f, Screen.width, Screen.height);
+	}
+
+	void Update()
+	{
+		if (sceneStarting) {
+			StartScene();
+		}
+	}
+
+	void FadeToClear()
+	{
+		guiTexture.color = Color.Lerp (GUITexture.color, Color.clear, fadeSpeed * Time.deltaTime);
+	}
+
+	void FadeToBlack()
+	{
+		guiTexture.color = Color.Lerp (GUITexture.color, Color.black, fadeSpeed * Time.deltaTime);
+	}
+
+	void StartScene()
+	{
+		FadeToClear();
+
+		if (guiTexture.color.a <= 0.05f) {
+			guiTexture.color = Color.clear;
+			guiTexture.enabled = false;
+			sceneStarting = false;
+		}
+	}
+
+	public void EndScene()
+	{
+		guiTexture.enabled = true;
+		FadeToBlack ();
+
+		if (guiTexture.color.a >= 0.95f) {
+			Application.LoadLevel(1);
+		}
+	}
+}
