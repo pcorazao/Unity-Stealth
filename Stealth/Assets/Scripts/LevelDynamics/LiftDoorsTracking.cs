0b2e69141dd87c017640328a19b29804c93316b7
Peter Corazao <pcorazao@nc2media.com>
2015-03-24 13:03:05 -0500

    added exit doors script

--- Stealth/Assets/Scripts/LevelDynamics/LiftDoorsTracking.cs
+++ Stealth/Assets/Scripts/LevelDynamics/LiftDoorsTracking.cs
@@ -15,10 +15,10 @@ public class LiftDoorsTracking : MonoBehaviour {
 
 	void Awake()
 	{
-		leftOuterDoor = GameObject.Find ("door_exitOuter_left_001").transform;
-		rightOuterDoor = GameObject.Find ("door_exitOuter_right_001").transform;
-		leftInnerDoor = GameObject.Find ("door_exitInner_left_001").transform;
-		rightInnerDoor = GameObject.Find ("door_exitInner_right_001").transform;
+		leftOuterDoor = GameObject.Find ("door_exit_outer_left_001").transform;
+		rightOuterDoor = GameObject.Find ("door_exit_outer_right_001").transform;
+		leftInnerDoor = GameObject.Find ("door_exit_inner_left_001").transform;
+		rightInnerDoor = GameObject.Find ("door_exit_inner_right_001").transform;
 
 		leftClosePosx = leftInnerDoor.position.x;
 		rightClosePosx = rightInnerDoor.position.x;
---
+++ Stealth/Assets/Scripts/LevelDynamics/LiftTrigger.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LiftTrigger : MonoBehaviour {
+
+	public float timeToDoorClose = 2f;
+	public float timeToLiftStart = 3f;
+	public float timeToEndLevel = 6f;
+	public float liftSpeed = 3f;
+
+	private GameObject player;
+	private Animator playerAnim;
+	private HashIDs hash;
+	private CameraMovement camMovement;
+	private SceneFadeInOut sceneFadeInOut;
+	private LiftDoorsTracking liftDoorsTracking;
+	private bool playerInLift;
+	private float timer;
+
+	void Awake()
+	{
+		player = GameObject.FindGameObjectWithTag (Tags.player);
+		playerAnim = player.GetComponent<Animator> ();
+		hash = GameObject.FindGameObjectWithTag (Tags.gameController).GetComponent<HashIDs> ();
+		camMovement = Camera.main.gameObject.GetComponent<CameraMovement> ();
+		sceneFadeInOut = GameObject.FindGameObjectWithTag (Tags.fader).GetComponent<SceneFadeInOut> ();
+		liftDoorsTracking = this.gameObject.GetComponent<LiftDoorsTracking> ();
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject == player) {
+			playerInLift = true;
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.gameObject == player) {
+			playerInLift = false;
+			timer = 0;
+		}
+	}
+
+	void Update()
+	{
+		if (playerInLift)
+			LiftActivation ();
+
+		if (timer < timeToDoorClose) {
+			liftDoorsTracking.DoorFollowing ();
+		} else {
+			liftDoorsTracking.CloseDoors();
+		}
+	}
+
+	void LiftActivation()
+	{
+		var audio = this.gameObject.GetComponent<AudioSource> ();
+
+		timer += Time.deltaTime;
+		if (timer >= timeToLiftStart) {
+			playerAnim.SetFloat(hash.speedFloat, 0f);
+			camMovement.enabled = false;
+			player.transform.parent = transform;
+
+			transform.Translate(Vector3.up * liftSpeed * Time.deltaTime);
+
+			if(!audio.isPlaying)
+				audio.Play();
+
+			if(timer >= timeToEndLevel)
+				sceneFadeInOut.EndScene();
+		}
+	}
+}
